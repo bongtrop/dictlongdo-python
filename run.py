8c171318dc5e5ba030bb6a11a3516ae012fdbe24
bongtrop <bongtrop@gmail.com>
2016-06-20 22:53:43 +0700

    change query string go path

--- run.py
+++ run.py
@@ -15,16 +15,39 @@ from twisted.web.server import Site
 from twisted.web.resource import Resource
 from twisted.internet import reactor
 
-class Longdo(Resource):
-    isLeaf = True
+class L(Resource):
+    def __init__(self, word):
+        Resource.__init__(self)
+        self.word = word
 
     def render_GET(self, request):
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
-        if "word" not in request.args:
-            request.setResponseCode(402)
-            return json.dumps({"error": "Please define args word."})
+        return json.dumps(pydictlongdo.translate(self.word))
 
-        return json.dumps(pydictlongdo.translate(request.args["word"][0]))
+class Longdo(Resource):
+    def getChild(self, name, request):
+        if len(name)==0:
+            return self
+
+        return L(name)
+
+    def render_GET(self, request):
+        request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+        request.setResponseCode(402)
+        return json.dumps({"error": "Please define args word."})
+
+
+
+class Novels(Resource):
+    def getChild(self, name, request):
+        if len(name)==0:
+            return self
+
+        return Chapters(name)
+
+    def render_GET(self, request):
+        request.setHeader("content-type", "application/json")
+        return json.dumps(wuxiaworld.getNovels())
 
 
 resource = Longdo()
